6047800650e6627a6cf2eca158e27ba17a8f8fc8
JoseCima <josegcima@gmail.com>
2022-02-17 21:04:04 -0500

    Layout Constraint - Primer Commit (Creación del Proyecto)

---
+++ app/src/main/java/com/example/a_saludo_desarrollador/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a_saludo_desarrollador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
